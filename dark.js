7725f28f06b2246ff2e6ae3115f0cb2074bc4466
dhdhagai <84077442+dhdhagai@users.noreply.github.com>
2023-10-23 23:34:48 +0530

    Updated Website with many new features
    Added favicon.ico webmanifest and other favicon images for logo.
    added header and footer into seperate files
    Dark Mode, once activated with a single click, seamlessly persists across all pages you visit.
    updated tables to now take less space and added dark mode into the previously disabled pages

--- dark.js
+++ dark.js
@@ -1,59 +1,197 @@
-const sw = document.querySelector("[sw]")
-const card = document.getElementsByClassName("card")
-const navbar = document.getElementsByClassName("container")
-const nav = document.getElementsByTagName("a")
-const div = document.getElementsByTagName("div")
-
-sw.addEventListener('click', () => {
-        if (sw.checked) {
-        document.body.style.background = "#2c3e50";
-
-        // Change the background and text color of the body
-        document.body.style.backgroundColor = "#2c3e50";
-        document.body.style.color = "white";
-
-        for (let item of card) {
-            item.style.backgroundColor = "#2c3e50";
-            item.style.color = "white";
-        }
-
-        for (let item of navbar) {
-            item.style.backgroundColor = "white"; // Set the background color to white
-            item.style.color = "#2c3e50"; // Set the text color to the body color
-        }
-
-        for (let item of nav) {
-
-            item.style.color = "white";
-        }
-        for (let item of div) {
-            item.style.backgroundColor = "#2c3e50";
-            item.style.color = "white";
-        }
-    }else{
-        document.body.style.background = "#2c3e50";
-
-// Change the background and text color of the body
-document.body.style.backgroundColor = "#FFFFFF";
-document.body.style.color = "black";
-
-for (let item of card) {
-    item.style.backgroundColor = "#FFFFFF";
-    item.style.color = "black";
-}
-
-for (let item of navbar) {
-    item.style.backgroundColor = "black"; // Set the background color to white
-    item.style.color = "#FFFFFF"; // Set the text color to the body color
-}
-
-for (let item of nav) {
-
-    item.style.color = "black";
-}
-for (let item of div) {
-    item.style.backgroundColor = "#FFFFFF";
-    item.style.color = "black";
-}
+let sw = document.querySelector("[sw]");
+let card = document.getElementsByClassName("card");
+let navbar = document.getElementsByClassName("container");
+let nav = document.getElementsByTagName("a");
+let div = document.getElementsByTagName("div");
+let tbl = document.getElementById("table")
+function toggleDarkMode(){
+    if(localStorage.getItem("darkMode") == "enabled"){
+        sw.checked = true
+        enableDarkMode()
+    }else if(localStorage.getItem("darkMode") == "disabled"){
+        sw.checked = false
+        disableDarkMode()
     }
-});
+}
+
+
+
+
+
+function enableDarkMode() {
+  document.body.style.background = "#2c3e50";
+  document.getElementById("drpdwn").classList.toggle("dropdown-menu-dark")
+  // Change the background and text color of the body
+  document.body.style.backgroundColor = "#2c3e50";
+  document.body.style.color = "white";
+
+  for (let item of card) {
+    item.style.backgroundColor = "#2c3e50";
+    item.style.color = "white";
+  }
+
+  for (let item of navbar) {
+    item.style.backgroundColor = "white"; // Set the background color to white
+    item.style.color = "#2c3e50"; // Set the text color to the body color
+  }
+
+  for (let item of nav) {
+    item.style.color = "white";
+  }
+  for (let item of div) {
+    item.style.backgroundColor = "#2c3e50";
+    item.style.color = "white";
+  }
+  document.getElementById("table").classList.add("table-dark");
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+function disableDarkMode() {
+  document.body.style.background = "#2c3e50";
+  document.getElementById("drpdwn").classList.remove("dropdown-menu-dark")
+  // Change the background and text color of the body
+  document.body.style.backgroundColor = "#FFFFFF";
+  document.body.style.color = "white";
+
+  for (let item of card) {
+    item.style.backgroundColor = "#FFFFFF";
+    item.style.color = "white";
+  }
+
+  for (let item of navbar) {
+    item.style.backgroundColor = "white"; // Set the background color to white
+    item.style.color = "#FFFFFF"; // Set the text color to the body color
+  }
+
+  for (let item of nav) {
+    if (
+      item.classList.forEach((element) => {
+        return element == "active";
+      })
+    ) {
+      item.style.color = "white";
+    } else {
+      item.style.color = "black";
+    }
+  }
+  for (let item of div) {
+    item.style.backgroundColor = "#FFFFFF";
+    item.style.color = "black";
+  }
+  tbl.classList.remove("table-dark");
+}
+
+
+
+
+
+
+
+
+
+
+
+
+function addEv() {
+  sw.addEventListener("click", () => {
+    if (sw.checked) {
+      localStorage.setItem("darkMode", "enabled");
+      toggleDarkMode()
+      
+    } else {
+      localStorage.setItem("darkMode", "disabled");
+      toggleDarkMode()
+    }
+  });
+  
+}
+
+
+
+
+
+
+
+
+
+
+
+function includeHTML() {
+  var z, i, elmnt, file, xhttp;
+  function setActiveAndBuluClass() {
+    const currentPage = window.location.pathname;
+    const dropdownLinks = document.querySelectorAll(".dropdown-item");
+    const servicesText = document.querySelector(".nav-link.serv");
+    const navLinks = document.querySelectorAll(".nav-link");
+    navLinks.forEach((link) => {
+      const linkPath = link.getAttribute("href");
+      if (currentPage === linkPath) {
+        link.classList.add("active", "bulu");
+      } else {
+        link.classList.remove("active", "bulu");
+      }
+    });
+    dropdownLinks.forEach((link) => {
+      const linkPath = link.getAttribute("href");
+      link.classList.remove("active", "bulu");
+      if (currentPage === linkPath) {
+        link.classList.add("active", "bulu");
+        servicesText.classList.add("active", "bulu");
+      }
+    });
+  }
+
+  z = document.getElementsByTagName("*");
+  for (i = 0; i < z.length; i++) {
+    elmnt = z[i];
+
+    file = elmnt.getAttribute("w3-include-html");
+    if (file) {
+      xhttp = new XMLHttpRequest();
+      xhttp.onreadystatechange = function () {
+        if (this.readyState == 4) {
+          if (this.status == 200) {
+            elmnt.innerHTML = this.responseText;
+
+            setActiveAndBuluClass();
+          }
+          if (this.status == 404) {
+            elmnt.innerHTML = "Page not found.";
+          }
+
+          elmnt.removeAttribute("w3-include-html");
+          includeHTML();
+        }
+      };
+      xhttp.open("GET", file, true);
+      xhttp.send();
+
+      return;
+    }
+  }
+
+  setActiveAndBuluClass();
+
+  sw = document.querySelector("[sw]");
+  card = document.getElementsByClassName("card");
+  navbar = document.getElementsByClassName("container");
+  nav = document.getElementsByTagName("a");
+  div = document.getElementsByTagName("div");
+  tbl = document.getElementById("table")
+  addEv();
+  toggleDarkMode()
+}
+
+// Call includeHTML when the page loads
+includeHTML();
