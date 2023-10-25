e1758a097540436a9b72d33ac6cab2247c338e3a
dhdhagai <84077442+dhdhagai@users.noreply.github.com>
2023-10-25 23:45:52 +0530

    Using @coliff's Dark mode( Dark mode changes complete)

--- dark.js
+++ dark.js
@@ -1,16 +1,24 @@
-let sw = document.querySelector("[sw]");
-let card = document.getElementsByClassName("card");
-let navbar = document.getElementsByClassName("container");
-let nav = document.getElementsByTagName("a");
-let div = document.getElementsByTagName("div");
-let tbl = document.getElementById("table")
-let ourserv = document.getElementById("ourserv")
-let label = document.querySelectorAll(".form-label")
-function addEv() {
-  sw.addEventListener("click", () => {
-document.getElementById("content").classList.toggle('dark-mode');
+if(window.location.pathname=="/"){
+window.location = "/index.html"
 }
-  )}
+function DarkModeToggle() {
+  const darkSwitch = localStorage.getItem("darkSwitch");
+  const navLinks = document.querySelectorAll(".nav-link");
+
+
+
+  if (darkSwitch === null) {
+    navLinks.forEach(element => {
+      element.classList.remove("text-white");
+      document.getElementById("drpdwn").classList.remove(".dropdown-menu-dark")
+    });
+  } else if (darkSwitch === "dark"){
+    navLinks.forEach(element => {
+      element.classList.add("text-white");
+      document.getElementById("drpdwn").classList.add(".dropdown-menu-dark")
+    });
+  }
+}
 function includeHTML() {
   var z, i, elmnt, file, xhttp;
   function setActiveAndBuluClass() {
@@ -19,8 +27,8 @@ function includeHTML() {
     const servicesText = document.querySelector(".nav-link.serv");
     const navLinks = document.querySelectorAll(".nav-link");
     navLinks.forEach((link) => {
-      const linkPath = link.getAttribute("href");
-      if (currentPage === linkPath) {
+       linkPath = link.getAttribute("href");
+      if (currentPage === linkPath || (linkPath === "/")) {
         link.classList.add("active", "bulu");
       } else {
         link.classList.remove("active", "bulu");
@@ -28,10 +36,13 @@ function includeHTML() {
     });
     dropdownLinks.forEach((link) => {
       const linkPath = link.getAttribute("href");
+      
       link.classList.remove("active", "bulu");
-      if (currentPage === linkPath) {
+
+      if (currentPage === linkPath ) {
         link.classList.add("active", "bulu");
         servicesText.classList.add("active", "bulu");
+        
       }
     });
   }
@@ -66,18 +77,15 @@ function includeHTML() {
   }
 
   setActiveAndBuluClass();
-
-  sw = document.querySelector("[sw]");
-  card = document.getElementsByClassName("card");
-  navbar = document.getElementsByClassName("container");
-  nav = document.getElementsByTagName("a");
-  div = document.getElementsByTagName("div");
-  tbl = document.getElementById("table")
-  ourserv = document.getElementById("ourserv")
-  label = document.querySelectorAll(".form-label")
-addEv()
-
+toggleDarkModeButton = document.querySelector("[sw1]");
+DarkModeToggle()
+toggleDarkModeButton.addEventListener("click", () => {
+  setTimeout(() => {
+    DarkModeToggle()
+  }, 100);
+})
 }
 
 // Call includeHTML when the page loads
 includeHTML();
+
